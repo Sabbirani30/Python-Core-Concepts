37f4bd0bcd595833629c4acab426b6fbb793e6ef
Sabbir Hossain Anik <sabbirhossainlikhon0160@gmail.com>
2026-05-27 19:21:35 +0600

    feat: added variable practice

--- main.py
+++ main.py
@@ -11,6 +11,38 @@ College=input("Enter your college name:")
 University=input("Enter your university name:")
 FatherName=input("Enter your father name:")
 MotherName=input("Enter your mother name:")
+yourphoneNumber=input("Enter Your Phone Number")
+
+# Variable Practice
+
+Name = "Anik"
+user="My"
+user1="Name"
+user2="is"
+
+print(user , user1 , user2 , Name)
+
+anikIsBigMan="Who"
+print(anikIsBigMan,"is Big Man?")
+
+# Multiple Assignment
+A,B,C,D=34,40,50,60
+print(A)
+print(B)
+print(C)
+print(D)
+
+# Chained Assignmen
+
+A=B=C=5000
+print(A)
+print(B)
+print(C)
+print(D)
+
+
+
+
 
 
 
