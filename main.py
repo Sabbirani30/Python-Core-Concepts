a498ba619fc6fd43d2a46bab31348d6e16ab6fb6
Sabbir Hossain Anik <sabbirhossainlikhon0160@gmail.com>
2026-05-29 22:05:58 +0600

    feat: added string methods and practice

--- main.py
+++ main.py
@@ -1,44 +1,44 @@
 # Output Function
 print ('Hello World')
 
-# Input function
+#Input function
 
-# Name =input("Enter your name:")
-# FullName=input("Enter your full name:")
-# Age=input("Enter your age:")
-# School=input("Enter your school name:")
-# College=input("Enter your college name:")
-# University=input("Enter your university name:")
-# FatherName=input("Enter your father name:")
-# MotherName=input("Enter your mother name:")
-# yourphoneNumber=input("Enter Your Phone Number")
+Name =input("Enter your name:")
+FullName=input("Enter your full name:")
+Age=input("Enter your age:")
+School=input("Enter your school name:")
+College=input("Enter your college name:")
+University=input("Enter your university name:")
+FatherName=input("Enter your father name:")
+MotherName=input("Enter your mother name:")
+yourphoneNumber=input("Enter Your Phone Number")
 
-# Variable Practice
+#Variable Practice
 
-# Name = "Anik"
-# user="My"
-# user1="Name"
-# user2="is"
-#
-# print(user , user1 , user2 , Name)
-#
-# anikIsBigMan="Who"
-# print(anikIsBigMan,"is Big Man?")
+Name = "Anik"
+user="My"
+user1="Name"
+user2="is"
 
-# Multiple Assignment
-# A,B,C,D=34,40,50,60
-# print(A)
-# print(B)
-# print(C)
-# print(D)
+print(user , user1 , user2 , Name)
+
+anikIsBigMan="Who"
+print(anikIsBigMan,"is Big Man?")
+
+#Multiple Assignment
+A,B,C,D=34,40,50,60
+print(A)
+print(B)
+print(C)
+print(D)
 
 # Chained Assignmen
 
-# A=B=C=5000
-# print(A)
-# print(B)
-# print(C)
-# print(D)
+A=B=C=5000
+print(A)
+print(B)
+print(C)
+print(D)
 
 # String Practice
 
