86fa4b464928a7cd5ff89843674af860221d51e9
Sabbir Hossain Anik <sabbirhossainlikhon0160@gmail.com>
2026-06-02 02:18:32 +0600

    feat: implemented basic python data types and conversion

---
+++ Data Type.py
@@ -0,0 +1,34 @@
+#Numerical (Integer)
+A=298
+print(type(A))
+B=546
+print(type(B))
+
+#Float
+
+Name=2087.9
+print(type(Name))
+Anik=24.9
+print(type(Anik))
+
+# complex
+
+A=9+12j
+print(type(A))
+B=5J
+print(type(B))
+
+# Boolean
+A=90>10
+print(A)
+print(type(A))
+
+B=1098<894508
+print(B)
+print(type(B))
+
+C=108749396==87993
+print(C)
+
+
+
