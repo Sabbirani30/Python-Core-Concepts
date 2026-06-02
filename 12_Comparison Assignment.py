c0fc58fc5765af0c3e9008a8923bc206d17a2645
Sabbir Hossain Anik <sabbirhossainlikhon0160@gmail.com>
2026-06-02 20:35:45 +0600

    "feat: implement comparison operators"

---
+++ 12_Comparison Assignment.py
@@ -0,0 +1,17 @@
+Anik=900
+Sabbir=50000
+Likhon=17633
+sabrin=900
+
+print(Anik)
+print(Sabbir)
+print(Likhon)
+print(sabrin)
+print(Anik>Likhon)
+print(Anik<Likhon)
+print(Likhon<=Anik)
+print(Likhon>=Anik)
+print(Anik==sabrin)
+print(Anik!=Likhon)
+print(Anik<Likhon<Sabbir)
+print(Anik==sabrin>Sabbir)
