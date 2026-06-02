81f85780c67fe1c0b86be6307e3bd2ed2812bbf6
Sabbir Hossain Anik <sabbirhossainlikhon0160@gmail.com>
2026-06-02 15:43:44 +0600

    feat: add implementation for List and Tuple data structures

---
+++ Python_Basic_Concepts_and_Practice/List.py
@@ -0,0 +1,9 @@
+Firstlist = [1,2,3,4,5,6,7,8,9,10]
+print(Firstlist)
+print(type(Firstlist))
+
+secondlist = ["anik","Anik","Sabbir","Anki","Likhon",1,2,3,4,5,6,7,8,9,10]
+print(secondlist)
+print(type(secondlist))
+print(secondlist[0])
+print((secondlist[6]))
---
+++ Tuple.py
@@ -0,0 +1,12 @@
+FirstTuple= ("Anik","Sabbir","Likhon","34","Anik","Likhon")
+print(FirstTuple)
+print(type(FirstTuple))
+print(FirstTuple)
+print(type(FirstTuple))
+
+
+_2ndTuple=("Anik","Sabbir","Likhon")
+print(FirstTuple)
+print(type(FirstTuple))
+print(_2ndTuple)
+print(type(_2ndTuple))
