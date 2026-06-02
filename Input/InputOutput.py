9faffc4dbaee3d438ce6f30baf6d3c2deaca191c
Sabbir Hossain Anik <sabbirhossainlikhon0160@gmail.com>
2026-06-02 10:00:26 +0600

    refactor: separate input-output and variable logic into dedicated files and clean main.py

---
+++ Input/InputOutput.py
@@ -0,0 +1,12 @@
+#Input function
+
+Name =input("Enter your name:")
+FullName=input("Enter your full name:")
+Age=input("Enter your age:")
+School=input("Enter your school name:")
+College=input("Enter your college name:")
+University=input("Enter your university name:")
+FatherName=input("Enter your father name:")
+MotherName=input("Enter your mother name:")
+yourphoneNumber=input("Enter Your Phone Number")
+
---
+++ Variable.py
@@ -0,0 +1,26 @@
+#Variable Practice
+
+Name = "Anik"
+user="My"
+user1="Name"
+user2="is"
+
+print(user , user1 , user2 , Name)
+
+anikIsBigMan="Who"
+print(anikIsBigMan,"is Big Man?")
+
+#Multiple Assignment
+A,B,C,D=34,40,50,60
+print(A)
+print(B)
+print(C)
+print(D)
+
+# Chained Assignment
+
+A=B=C=5000
+print(A)
+print(B)
+print(C)
+print(D)
