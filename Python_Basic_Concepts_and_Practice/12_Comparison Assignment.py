0ae2a9d97b75cca2dd855854584b6cae6a8d20b0
Sabbir Hossain Anik <sabbirhossainlikhon0160@gmail.com>
2026-06-02 18:46:55 +0600

    feat: implement comparison operators in 12_Comparison Assignment.py

---
+++ Python_Basic_Concepts_and_Practice/12_Comparison Assignment.py
@@ -0,0 +1,20 @@
+Anik=1000
+Sabbir=1400
+Likhon=500
+Sabrin =500
+
+print(Anik<Likhon)
+print(Anik>Likhon)
+print(Anik==Likhon)
+print(Anik>Sabbir)
+print(Anik>Sabrin)
+print(Likhon==Sabrin)
+print(Likhon>=Sabbir)
+print(Likhon>=Sabrin)
+print(Likhon*Sabrin)
+print(Likhon**Sabrin)
+print(Likhon%Sabrin)
+print(Anik//Sabrin)
+print(Anik%Sabrin)
+print(Anik//Sabrin)
+print(Anik/Sabrin)
