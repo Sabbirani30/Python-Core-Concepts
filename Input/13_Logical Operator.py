166be45d2978d7bd96a606a36be7d5d56d710ec1
Sabbir Hossain Anik <sabbirhossainlikhon0160@gmail.com>
2026-06-03 19:56:49 +0600

    feat: practice logical operators and organize files

---
+++ Input/13_Logical Operator.py
@@ -0,0 +1,70 @@
+# and Operator
+
+Name="Sabbir Hossain Anik"
+University= "Southeast University"
+Age=23
+
+if Name == "Sabbir Hossain Anik" and Age== 23:
+    print("Hossain Anik")
+else:
+    print(" Anik")
+
+Name="Sabbir Hossain Anik"
+University= "Southeast University"
+Age=23
+
+if Name == "Sabbir Hossain Anik" and Age== 20:
+    print("Hossain Anik")
+else:
+    print("Error ")
+
+Name="Sabbir Hossain Anik"
+University= "Southeast University"
+Age=23
+
+if Name == "Sabbir Hossain Anik" and University== "Southeast University":
+    print("Student ")
+else:
+    print(" Not A student ")
+
+
+# or OPerator
+
+
+Name="Sabbir Hossain Anik"
+University= "Southeast University"
+Age=23
+
+if Name == "Sabbir Hossain Anik" or Age== 20:
+    print("You are a genius  ")
+else:
+    print("You are not a genius  ")
+
+if Name == "Sabbir Hossain nik" or Age== 20:
+    print("You are a genius  ")
+else:
+    print("You are not a genius  ")
+
+if Name == "Sabbir Hossain Anik" or University== "Southeast University":
+    print("You are a Winner  ")
+else:
+    print("You are not a Winner  ")
+
+# not Operator
+
+Name="Sabbir Hossain Anik"
+University= "Southeast University"
+Age=23
+
+if Name == "Sabbir Hossain Anik" and not Age== 22:
+    print("You are a Real Men  ")
+else:
+    print("You are not a Real Men  ")
+
+if Name == "Sabbir Hossain Anik" and not Age== 23:
+    print("You are a  Men  ")
+else:
+    print("You are not a  Men  ")
+
+
+
