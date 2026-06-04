d6f66db83c477b1ee79b6817b25b565a7a7cc1ad
Sabbir Hossain Anik <sabbirhossainlikhon0160@gmail.com>
2026-06-04 18:45:12 +0600

    feat: add Bitwise Operator practice code

---
+++ Python_Basic_Concepts_and_Practice/14_Bitwise Operator.py
@@ -0,0 +1,27 @@
+A=10
+B=20
+C=30
+
+print(A & B)
+print(A | B)
+print(A ^ B)
+print(A << B)
+print(A >> B)
+print(~ B)
+
+print (A & C)
+print(A | C)
+print(A ^ C)
+print(A >> C)
+print(~ C)
+print(A << C)
+print(A >> C)
+print(~ C)
+
+print(B & C)
+print(A | C)
+print(A ^ C)
+print(B & C)
+print(A >> C)
+print(~ C)
+print(A & B)
