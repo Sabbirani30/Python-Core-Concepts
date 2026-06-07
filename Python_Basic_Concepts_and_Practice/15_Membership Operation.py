9056c58f8b99d6536e4e6dc193b00b0cf4739aee
Sabbir Hossain Anik <sabbirhossainlikhon0160@gmail.com>
2026-06-07 19:48:12 +0600

    feat: add Membership Operator practice

---
+++ Python_Basic_Concepts_and_Practice/15_Membership Operation.py
@@ -0,0 +1,31 @@
+Fruits= ["Mango","Banana","Cucumber"]
+print(Fruits)
+print("Mango " in Fruits)
+print("Banana" in Fruits)
+print("Cucumber" not in  Fruits)
+print("Mango" in Fruits , "Banana" in Fruits)
+
+
+students = ["Anik", "Nabil", "Tamim", "Fahad", "Shifad", "Rohan"]
+print(students)
+print("Nabil" in students)
+print("Tamim" in students)
+print("Shifad" in students)
+print("Fahad" in students)
+print("Rohan" in students)
+print("Anik" in students)
+print("Likhon" in students)
+print("Tamima" in students)
+print("Fardin" not in students)
+print("Farabi" not in students)
+
+days = ["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+print(days)
+print("saturday" in days)
+print("sunday" in days)
+print("Monday" in days)
+print("Tuesday" in days)
+print("Wednesday" in days)
+print("Thursday" in days)
+print("Friday" in days)
+
