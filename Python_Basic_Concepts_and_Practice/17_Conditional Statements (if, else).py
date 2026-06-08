04e4fec68f8c40fc7dfba182088e95c6563e9b64
Sabbir Hossain Anik <sabbirhossainlikhon0160@gmail.com>
2026-06-08 23:24:43 +0600

    feat: add Conditional Statements practice

---
+++ Python_Basic_Concepts_and_Practice/17_Conditional Statements (if, else).py
@@ -0,0 +1,16 @@
+Motu= 100
+Patlu=50
+
+if Motu>Patlu:
+    print("Motu is greater than Patlu")
+else:
+    print("Motu is less than Patlu")
+
+Anik=4590
+Sabbir=5639
+
+if Anik<Sabbir:
+    print("Anik is less than Sabbir")
+else:
+    print("Anik is greater than Sabbir")
+
