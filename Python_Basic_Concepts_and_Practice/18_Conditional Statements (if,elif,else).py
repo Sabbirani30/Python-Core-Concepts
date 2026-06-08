78bc4a8a717e335cf9e0a4d98bfb0d8c107c84be
Sabbir Hossain Anik <sabbirhossainlikhon0160@gmail.com>
2026-06-09 04:56:32 +0600

    "update: finalize notification system logic with correct ranges"

--- Python_Basic_Concepts_and_Practice/18_Conditional Statements (if,elif,else).py
+++ Python_Basic_Concepts_and_Practice/18_Conditional Statements (if,elif,else).py
@@ -45,6 +45,20 @@ elif Marks>=33:
 else:
     print("F")
 
+#Notification_System
+
+notification = int(input("Enter Days left: "))
+
+if notification >= 30:
+    print("Your data is active and running smoothly.")
+elif notification >= 25:
+    print("Your data is almost finished, please recharge soon.")
+elif notification >= 20:
+    print("Your data is about to run out.")
+elif notification >= 15:
+    print("Half of your package validity is over.")
+else:
+    print("Your data is over or ending soon.")
 
 
 
