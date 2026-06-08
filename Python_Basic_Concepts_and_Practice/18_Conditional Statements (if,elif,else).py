5433391a03b757ff4068a391bec20f0942d0f293
Sabbir Hossain Anik <sabbirhossainlikhon0160@gmail.com>
2026-06-08 23:35:00 +0600

    feat: implement conditional logic and if-elif-else

---
+++ Python_Basic_Concepts_and_Practice/18_Conditional Statements (if,elif,else).py
@@ -0,0 +1,34 @@
+Likhon=100
+Sabbir=200
+Anik=300
+
+if Likhon>Sabbir:
+    print("Anik is greater than Sabbir")
+elif Anik>Sabbir:
+    print("Anik is less than Sabbir")
+else:
+    print("Anik is equal to Sabbir")
+
+
+Likhon=1000
+Sabbir=200
+Anik=3000
+
+if Likhon>Sabbir:
+    print("Anik is greater than Sabbir")
+    print("Anik is less than Sabbir")
+    print("Anik is equal to Sabbir")
+elif Sabbir>Likhon:
+    print("Sabbir is greater than Sabbir")
+    print("Sabbir is less than Sabbir")
+    print("Sabbir is equal to Sabbir")
+else:
+    print("Sabbir is equal to Sabbir")
+    print("Sabbir is less than Sabbir")
+    print("Sabbir is equal to Sabbir")
+
+
+
+
+
+
