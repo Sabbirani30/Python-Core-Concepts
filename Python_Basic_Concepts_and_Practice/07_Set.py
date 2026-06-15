325acbc18ebe1f0075996c3dfada87f180145120
Sabbir Hossain Anik <sabbirhossainlikhon0160@gmail.com>
2026-06-15 22:39:50 +0600

    update: add set examples and document unordered behavior with comments

--- Python_Basic_Concepts_and_Practice/07_Set.py
+++ Python_Basic_Concepts_and_Practice/07_Set.py
@@ -9,3 +9,11 @@ print(type(Secondset))
 Thirdset={"Anik",24,65,"Southeast",294}
 print(Thirdset)
 print(type(Thirdset))
+
+Fourthset={"Anik",24,65,"Southeast",294}
+print(Fourthset)
+print(type(Fourthset))
+
+Fifthset={"Anik",24,65,"Southeast"}
+print(Fifthset)
+print(type(Fifthset))  #(Output order is not guaranteed because sets are unordered.)Unorderd tai ekek time e ekek rokom order maintain kore output dey same code er jonno
