ecdded8d61729252c9ce0f03317a9eb88b20827d
Sabbir Hossain Anik <sabbirhossainlikhon0160@gmail.com>
2026-06-16 21:22:44 +0600

    feat: add list creation and indexing practice in 08_List.py

--- Python_Basic_Concepts_and_Practice/08_List.py
+++ Python_Basic_Concepts_and_Practice/08_List.py
@@ -7,3 +7,8 @@ print(secondlist)
 print(type(secondlist))
 print(secondlist[0])
 print((secondlist[6]))
+
+Thirdlist = [1,2,3,4,5,6,7,8,9,10]
+print(Thirdlist)
+print(type(Thirdlist))
+print(Thirdlist[0])
