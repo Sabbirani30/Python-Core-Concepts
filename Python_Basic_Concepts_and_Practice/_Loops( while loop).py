31e8d638820c0f351f3b8d7252417c0bf1779fed
Sabbir Hossain Anik <sabbirhossainlikhon0160@gmail.com>
2026-06-17 16:50:03 +0600

    feat: add student ID iteration logic using while loop

--- Python_Basic_Concepts_and_Practice/_Loops( while loop).py
+++ Python_Basic_Concepts_and_Practice/_Loops( while loop).py
@@ -3,3 +3,9 @@ while counter <=10:
     print("This is Loop Number",counter)
     counter=counter+1
     print("This Loop is Finished!")
+
+StudentID= 290
+while StudentID <=300:
+    print("Student ID Number is ",StudentID)
+    StudentID=StudentID+1
+    print("Student ID Number is Finished!!!!")
