653e4ee53e2028db85ec21bd6f856ab4bf5fb9d7
Sabbir Hossain Anik <sabbirhossainlikhon0160@gmail.com>
2026-06-18 18:24:48 +0600

    feat: implement list slicing and indexing for course data

--- Python_Basic_Concepts_and_Practice/08_List.py
+++ Python_Basic_Concepts_and_Practice/08_List.py
@@ -12,3 +12,12 @@ Thirdlist = [1,2,3,4,5,6,7,8,9,10]
 print(Thirdlist)
 print(type(Thirdlist))
 print(Thirdlist[0])
+
+Course=["Machine Learning","Django","FastApi","Python","RestApi"]
+print(Course)
+print(len(Course))  #The length of a list is measured using the len() function. (length Mapa hoy)
+print(Course[2]) # Access List (Nirdishto kono item dekhte chaile eivabe lekhte hoy)
+print(Course[:5]) # Range Of Index ( Range nirdharon kora hoy , koto Numer index theke koto number porjonto dekha jabe)
+print(Course[0:3])
+
+
